0f7006a90f4fb456d6bc1034d0de9265e5425eb6
unknown <tariqmuhanna14@gmail.com>
2022-06-10 18:53:14 -0500

    Created data acccess layer for service layer to interface to db

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -11,15 +12,14 @@ import java.util.List;
 public class StudentService {
     // Service/business layer
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mary",
-                        "maryjane@gmail.com",
-                        LocalDate.of(2000, Month.APRIL, 5),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
